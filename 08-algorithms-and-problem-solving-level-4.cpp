2b0fa15d0f97a450efa06afd1f3b4b37cecf5b4b
Faten Eid <fatenfarid1221@gmail.com>
2024-11-17 15:02:54 +0200

    Add project files.

---
+++ 08-algorithms-and-problem-solving-level-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "P01_NumberToText.h"
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+}
---
+++ P01_NumberToText.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+namespace P01_NumberToText {
+
+
+
+
+
+}
+
