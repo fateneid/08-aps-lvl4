398dcb3c804e8c54fdedaa60314dcdc31ba41868
Faten Eid <fatenfarid1221@gmail.com>
2024-11-21 16:28:18 +0200

    P01 profSol.

--- 08-algorithms-and-problem-solving-level-4.cpp
+++ 08-algorithms-and-problem-solving-level-4.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "P01_NumberToText.h"
+#include "P01_NumberToText_ProfSol.h"
 
 using namespace std;
 
 int main()
 {
-	P01_NumberToText::Task();
 
-
+	P01_NumberToText_ProfSol::Task();
 
 
 
