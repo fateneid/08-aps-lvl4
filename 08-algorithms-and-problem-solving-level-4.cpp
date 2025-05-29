89b0a95388a48ed79880108179a8cfa53c418692
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 05:05:41 +0300

    remove unnecessary lines and comments

--- 08-algorithms-and-problem-solving-level-4.cpp
+++ 08-algorithms-and-problem-solving-level-4.cpp
@@ -1,37 +1,4 @@
 #include <iostream>
-#include "P01_NumberToText.h"
-#include "P01_NumberToText_ProfSol.h"
-#include "P02_LeapYear.h"
-#include "P03_LeapYear_OneLineOfCode.h"
-#include "P04_NumberOfDaysHoursMinutesSecondsInAYear.h"
-#include "P05_NumberOfDaysHoursMinutesSecondsInAMonth.h"
-#include "P06_NumberOfDaysInAMonthShortLogic.h"
-#include "P07_DayName.h"
-#include "P08_MonthCalendar.h"
-#include "P09_YearCalendar.h"
-#include "P10_DaysFromTheBeginningOfYear.h"
-#include "P11_DateFromDayOrderInAYear.h"
-#include "P12_AddDaysToDate.h"
-#include "P13_Date1LessThanDate2.h"
-#include "P14_Date1EqualsToDate2.h"
-#include "P15_LastDayLastMonth.h"
-#include "P16_IncreaseDateByOneDay.h"
-#include "P17_DiffInDays.h"
-#include "P18_YourAgeInDays.h"
-#include "P19_DiffInDaysNegativeDays.h"
-#include "P20ToP32_IncreaseDateProblems.h"
-#include "P33To46_DecreaseDateProblems.h"
-#include "P47ToP53_MoreDateProblems.h"
-#include "P54_CalculateVacationDays.h"
-#include "P55_CalculateVacationReturnDate.h"
-#include "P56_IsDate1AfterDate2.h"
-#include "P57_CompareDateFunction.h"
-#include "P58_IsOverlapPeriods.h"
-#include "P59_PeriodLengthInDays.h"
-#include "P60_IsDateWithinPeriod.h"
-#include "P61_CountOverlapDays.h"
-#include "P62_ValidateDate.h"
-#include "P63AndP64_ReadPrintDateString.h"
 #include "P65_FormatDate.h"
 
 using namespace std;
@@ -39,21 +6,10 @@ using namespace std;
 int main()
 {
 
-	P65_FormatDate::Task();
 
+	P65_FormatDate::Task();
 
 	
 }
 
-/*
-How to use:
 
-1. Include the library.
-2. Use the corresponding namespace, which matches the library name.
-3. Call the 'Task' function to execute the problem.
-
-Each library is designed for a specific problem, 
-and its namespace contains all the necessary functions to solve it. 
-Simply include the library, access its functions through the namespace, 
-and run the problem by calling 'Task'.
-*/
